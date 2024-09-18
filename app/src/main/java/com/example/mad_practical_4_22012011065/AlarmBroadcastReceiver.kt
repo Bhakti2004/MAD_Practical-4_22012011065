4b9f09b5deb6b73f707560cfb9c30a4715c2c71d
Bhakti Kansagara <bhaktikansagara2004@gmail.com>
2024-09-18 10:04:14 +0530

    hihk

--- app/src/main/java/com/example/mad_practical_4_22012011065/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/example/mad_practical_4_22012011065/AlarmBroadcastReceiver.kt
@@ -3,11 +3,13 @@ package com.example.mad_practical_4_22012011065
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 
 class AlarmBroadcastReceiver : BroadcastReceiver() {
-
+    var mp:MediaPlayer?=null
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+
         TODO("AlarmBroadcastReceiver.onReceive() is not implemented")
+
     }
 }
--- app/src/main/java/com/example/mad_practical_4_22012011065/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_22012011065/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.mad_practical_4_22012011065
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.app.TimePickerDialog
+import android.content.Intent
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.TimePicker
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.sql.Time
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.zip.DataFormatException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,4 +28,62 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    fun getCurrentDateTime(): String{
+        val cal=Calendar.getInstance()
+        val df: DateFormat= SimpleDateFormat("MMM,dd yyyy hh:mm:ss a")
+        return df.format(cal.time)
+    }
+    fun getMillis(hour:Int,min:Int):Long
+    {
+        val setCalendar=Calendar.getInstance()
+        setCalendar[Calendar.HOUR_OF_DAY]=hour
+        setCalendar[Calendar.MINUTE]=min
+        setCalendar[Calendar.SECOND]=0
+        return setCalendar.timeInMillis
+
+    }
+    fun showTimerDialog(){
+        val cldr: Calendar = Calendar.getInstance()
+        val hour: Int = cldr.get(Calendar.HOUR_OF_DAY)
+        val minutes: Int = cldr.get(Calendar.MINUTE)
+        val picker=TimePickerDialog(
+            this,
+            { tp, sHour, sMinute -> sendDialogDataToActivity(sHour, sMinute)},
+            hour,
+            minutes,
+            false
+        )
+        picker.show()
+    }
+    fun setAlarm(millisTime: Long,str:String)
+    {
+        val intent= Intent(this,AlarmBroadcastReceiver::class.java)
+        intent.putExtra("Service1",str)
+        val pendingIntent=PendingIntent.getBroadcast(applicationContext,234324243,intent,PendingIntent.FLAG_UPDATE_CURRENT )
+        val alarmManager= getSystemService(ALARM_SERVICE) as AlarmManager
+        if(str=="Start")
+        {
+            if(alarmManager.canScheduleExactAlarms()) {
+                alarmManager.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    millisTime,
+                    pendingIntent
+                )
+            }
+        }
+        else if (str=="Stop")
+        {
+            alarmManager.cancel(pendingIntent);
+            sendBroadcast(intent)
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
